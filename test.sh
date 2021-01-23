a1a9b4db3123f2878df8240c8b5a7e2e9fbf7d22
Florian Paul Schmidt <mista.tapas@gmx.net>
2021-01-23 10:11:11 +0100

    fix stupidity

--- test.sh
+++ test.sh
@@ -1,11 +1,11 @@
 # set -e
 
-./lv2-ttl-generator.sh -m ttl -u http://fps.io/plugin -n "Simple Filter" -t simple-filter.ttl -b simple-filter.so -c 2 -p "-n foo -s foo" -p "-n bar -s bar" > test.ttl
+./lv2-ttl-generator.sh -m ttl -u http://fps.io/example -n "Example Plugin" -b example.so -c 2 -p "-n Gain -s gain -d 0.5 -i -0.0 -x 1.0 -l" -p '-n "Cutoff Frequency" -s cutoff -i 1.0 -x 20000.0 -d 2000.0 -h' > test.ttl
 
 lv2_validate test.ttl
 
-./lv2-ttl-generator.sh -m manifest-prefix -u http://fps.io/plugin -n "Simple Filter" -t test.ttl -b simple-filter.so -c 2 -p "-n foo -s foo" -p "-n bar -s bar" > test_manifest.ttl
-./lv2-ttl-generator.sh -m manifest -u http://fps.io/plugin -n "Simple Filter" -t test.ttl -b simple-filter.so -c 2 -p "-n foo -s foo" -p "-n bar -s bar" >> test_manifest.ttl
+./lv2-ttl-generator.sh -m manifest-prefix  > test_manifest.ttl
+./lv2-ttl-generator.sh -m ttl -u http://fps.io/example -t test.ttl >> test_manifest.ttl
 
 lv2_validate test_manifest.ttl
 
